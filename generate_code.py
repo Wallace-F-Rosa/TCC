5953c7dd70b239c140104e1627d20598a4941bef
wallace-f-rosa <wferancini@gmail.com>
2021-03-05 20:15:54 -0300

    leitura da rede e inicialização dos estados

--- generate_code.py
+++ generate_code.py
@@ -4,14 +4,16 @@ import sys
 def generateCudaCode(weights_file_path):
     # lendo pesos das redes
     weightsFile = open(weights_file_path, 'r')
-    fileContent = weights_file.readlines()
+    fileContent = weightsFile.readlines()
     weightsFile.close()
 
-    #definindo valores
-    networkSize = fileContent[0].split('\n')[0]
-    weightsSize = fileContent[1].split('\n')[1].split(' ')
+    # definindo valores
+    networkSize = int(fileContent[0].split('\n')[0])
+    weightsSize = fileContent[1].split('\n')[0].split(' ')
     # gerando código da rede
     code_file = open('tlf.cu', 'w+')
+    
+    # headers do código
     code_file.write('#include <iostream>\n')
     code_file.write('#include <chrono>\n')
     code_file.write('#include <ctime>\n')
@@ -19,15 +21,44 @@ def generateCudaCode(weights_file_path):
     code_file.write('#include <limits>\n')
     code_file.write('#include <stdio.h>\n')
     code_file.write('#include <stdlib.h>\n')
-    code_file.write('struct { ulonglong['+str(networkSize//64)+'] } estado;')
+
+    # estado é um vetor de inteiros
+    # cada bit representa um vértice
+    stateSize = networkSize//64
+    code_file.write('typedef ulonglong['+str(stateSize)+'] state;\n')
+
+    # cuda kernel recebe os estados aleatórios inicialmente, simulando N estados até o número de simulações fornecido
+    code_file.write('__global__ void network_simulation(state * randState, state * statef, unsigned long long SIMULATIONS) {\n')
+    code_file.write('   unsigned long long tid = threadIdx.x + blockIdx.x*blockDim.x;\n')
+    code_file.write('   state state0, state1;\n')
+    code_file.write('   if (tid < SIMULATIONS) {')
+    
+    # inicializando estados
+    for i in range(stateSize):
+        code_file.write('       state0['+str(i)+'] = randState['+str(i)+'];')
+
+    # TODO : gerar equações da rede
+    for i in range(networkSize) :
+        eq = '      state1['+str(i//64)+'] |= '
+        line = fileContent[2+i].split('\n')[0].split(' ')
+        for y in range(len(weightsSize)):
+            eq += ''
+            
+
+    # salva o estado inicial do atrator na memória global da gpu
+    for i in range(stateSize) :
+        code_file.write('   statef[tid][i] = state1[i];\n')
+    code_file.write('   }')
+    code_file.write('}')
     code_file.close()
 
 if __name__ == '__main__' :
     try :
-        weights_file_path = sys.argv[0]
+        weights_file_path = sys.argv[1]
+        print(weights_file_path)
         if not os.path.exists(weights_file_path) :
             print('Arquivo com pesos da rede não foi encontrado!')
         else:
             generateCudaCode(weights_file_path)
-    except :
-        print('Arquivo com pesos da rede deve ser fornecido como parâmetro do programa!')
+    except Exception as e:
+        print(e)
