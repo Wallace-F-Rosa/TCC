e86c08be7ca24da65f4c3d1f9c47a1a96f72a7ae
wallace-f-rosa <wferancini@gmail.com>
2021-03-06 19:09:16 -0300

    código de passos utilizando equações da rede

--- generate_code.py
+++ generate_code.py
@@ -9,7 +9,8 @@ def generateCudaCode(weights_file_path):
 
     # definindo valores
     networkSize = int(fileContent[0].split('\n')[0])
-    weightsSize = fileContent[1].split('\n')[0].split(' ')
+    weightsSize = [ int(x) for x in fileContent[1].split('\n')[0].split(' ')]
+    print(weightsSize)
     # gerando código da rede
     code_file = open('tlf.cu', 'w+')
     
@@ -27,6 +28,15 @@ def generateCudaCode(weights_file_path):
     stateSize = networkSize//64 + (networkSize%64 != 0)
     code_file.write('typedef ulonglong['+str(stateSize)+'] state;\n')
 
+    #função de comparação entre estados
+    code_file.write('bool equals(state a, state b) {\n')
+    code_file.write('   for (int i = 0; i < '+str(stateSize)+'; i++) {\n')
+    code_file.write('       if (a[i] != b[i])\n')
+    code_file.write('           return false;\n')
+    code_file.write('   }\n')
+    code_file.write('   return true\n')
+    code_file.write('}\n')
+
     # cuda kernel recebe os estados aleatórios inicialmente, simulando N estados até o número de simulações fornecido
     code_file.write('__global__ void network_simulation(state * randState, state * statef, unsigned long long SIMULATIONS) {\n')
     code_file.write('   unsigned long long tid = threadIdx.x + blockIdx.x*blockDim.x;\n')
@@ -36,14 +46,37 @@ def generateCudaCode(weights_file_path):
     # inicializando estados
     for i in range(stateSize):
         code_file.write('       state0['+str(i)+'] = randState['+str(i)+'];\n')
+        code_file.write('       state1['+str(i)+'] = 0;\n')
 
-    # TODO : gerar equações da rede
+    code_file.write('       do {\n')
+
+    # equações : 
+    # estadof[var//64] = 0
+    # estadof[var//64] |= ( ( ( (estado0[i//64] >> var) % 2 )*peso + ( (estado0[i//64] >> var) % 2 )*peso  ...) >= lim) << var;
+    # gerando equações do passo 1 (estado0 anda um passo)
     for i in range(networkSize) :
-        eq = '      state1['+str(i//64)+'] |= '
+        eq = '          state1['+str(i//64)+'] |= ( ( '
         line = fileContent[2+i].split('\n')[0].split(' ')
-        for y in range(len(weightsSize)):
-            eq += ''
+        for y in range(weightsSize[i]):
+            eq += '( ( state0['+str(i//64)+'] >> '+str(line[2*y])+') % 2 ) * '+str(line[2*y+1])
+        eq += ' ) >= '+str(line[len(line)-1])+' ) << '+str(i)+';\n'
+        code_file.write(eq)
+
+    # estado0 recebe estado1, andamos 1 passo com as equações da rede
+    for i in range(stateSize):
+        code_file.write('       state0['+str(i)+'] = state1['+str(i)+'];\n')
+
+    # aplicamos as equações novamente em estado1 para andar 2 passos
+    for i in range(networkSize) :
+        eq = '          state1['+str(i//64)+'] |= ( ( '
+        line = fileContent[2+i].split('\n')[0].split(' ')
+        for y in range(weightsSize[i]):
+            eq += '( ( state0['+str(i//64)+'] >> '+str(line[2*y])+') % 2 ) * '+str(line[2*y+1])
+        eq += ' ) >= '+str(line[len(line)-1])+' ) << '+str(i)+';\n'
+        code_file.write(eq)
             
+    # TODO : comparação entre estados (talvez possa ser um macro)
+    code_file.write('       } while(equals(state0, state1));\n')
 
     # salva o estado inicial do atrator na memória global da gpu
     for i in range(stateSize) :
@@ -53,12 +86,12 @@ def generateCudaCode(weights_file_path):
     code_file.close()
 
 if __name__ == '__main__' :
-    try :
-        weights_file_path = sys.argv[1]
-        print(weights_file_path)
-        if not os.path.exists(weights_file_path) :
-            print('Arquivo com pesos da rede não foi encontrado!')
-        else:
-            generateCudaCode(weights_file_path)
-    except Exception as e:
-        print(e)
+    # try :
+    weights_file_path = sys.argv[1]
+    print(weights_file_path)
+    if not os.path.exists(weights_file_path) :
+        print('Arquivo com pesos da rede não foi encontrado!')
+    else:
+        generateCudaCode(weights_file_path)
+# except Exception as e:
+    #     print(e)
