f2691c8a0083867108d87deac710028d36d349ac
Wallace Ferancini <wferancini@gmail.com>
2021-08-09 15:11:44 -0300

    fix mesmo atrator sempre

--- generate_code.py
+++ generate_code.py
@@ -91,7 +91,7 @@ def generateCudaCode(weights_file_path):
     
     # inicializando estados
     for i in range(stateSize):
-        code_file.write('       state0['+str(i)+'] = state1['+str(i)+'] = statef[tid*'+ str(stateSize) +' + '+ str(i) +'];\n')
+        code_file.write('       state1['+str(i)+'] = statef[tid*'+ str(stateSize) +' + '+ str(i) +'];\n')
 
     code_file.write('       do {\n')
 
@@ -102,8 +102,14 @@ def generateCudaCode(weights_file_path):
     # gerando equações do passo 1 (estado0 anda um passo)
     # FIXME: kernel não roda quando temos muitas instruções de equação
     # andamos 1 passo com estado 0
+    # inicializando estados
+    for i in range(stateSize):
+        code_file.write('       state0['+str(i)+'] = state1['+ str(i) +'];\n')
     code_file.write('           next_d(state0);\n')
 
+    # inicializando estados
+    for i in range(stateSize):
+        code_file.write('       state1['+str(i)+'] = state0['+ str(i) +'];\n')
     # andamos 2 passos com estado 1
     code_file.write('           next_d(state1);\n')
     code_file.write('           next_d(state1);\n')
