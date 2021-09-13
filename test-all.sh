95121a3d185f01e4b1880dc620c94fff0af3dc6f
Wallace Ferancini Rosa <wferancini@gmail.com>
2021-09-13 13:50:23 -0300

    scripts para facilitar testes de performance

---
+++ test-all.sh
@@ -0,0 +1,51 @@
+# !/bin/bash
+
+echo "Teste redes disponíveis em redes_reg_genes"
+
+echo "Cholesterol Regulatory Pathway"
+./test.sh "Cholesterol Regulatory Pathway"
+
+echo "Apoptosis network"
+./test.sh "Apoptosis network"
+
+echo "Guard Cell Abscisic Acid Signaling"
+./test.sh "Guard Cell Abscisic Acid Signaling"
+
+echo "Differentiation of T lymphocytes"
+./test.sh "Differentiation of T lymphocytes"
+
+echo "B bronchiseptica and T retortaeformis coinfectio"
+./test.sh "B bronchiseptica and T retortaeformis coinfectio"
+
+echo "MAPK Cancer Cell Fate Network"
+./test.sh "MAPK Cancer Cell Fate Network"
+
+echo "T-LGL Survival Network 2011"
+./test.sh "T-LGL Survival Network 2011"
+
+echo "PC12 Cell Differentiation"
+./test.sh "PC12 Cell Differentiation"
+
+echo "Bortezomib Responses in U266 Human Myelo"
+./test.sh "Bortezomib Responses in U266 Human Myelo"
+
+echo "HGF Signaling in Keratinocytes"
+./test.sh "HGF Signaling in Keratinocytes"
+
+echo "Glucose Repression Signaling 2009"
+./test.sh "Glucose Repression Signaling 2009"
+
+echo "Yeast Apoptosis"
+./test.sh "Yeast Apoptosis"
+
+echo "Lymphopoiesis Regulatory Network"
+./test.sh "Lymphopoiesis Regulatory Network"
+
+echo "IL-6 Signalling"
+./test.sh "IL-6 Signalling"
+
+echo "EGFR & ErbB Signaling"
+./test.sh "EGFR & ErbB Signaling"
+
+echo "signal transduction in fibroblasts"
+./test.sh "signal transduction in fibroblasts"
