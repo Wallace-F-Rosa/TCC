a4c5f74ae311569e86edc929b8fb2669958aa3ef
Wallace Ferancini Rosa <wferancini@gmail.com>
2021-09-13 14:00:31 -0300

    fix caminhos de script teste continuam errados

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@
 # B bronchiseptica and T retortaeformis coinfectio 
 mkdir -p "${1}"
 cd "${1}"
-python ../redes_reg_genes/redes_pesos/load_graph.py "redes_reg_genes/redes_bio/40-100/${1}/expr/expressions.ALL.txt" 1
-python generate_code.py expressions_pesos.txt
+python redes_reg_genes/redes_pesos/load_graph.py "redes_reg_genes/redes_bio/40-100/${1}/expr/expressions.ALL.txt" 1
+python TCC/generate_code.py expressions_pesos.txt
 nvcc -w -DTHREADS=256 -arch=sm_37 -Xcompiler -fopenmp -Wno-deprecated-gpu-targets -lcurand -o tlf tlf.cu
 ./tlf 1000 > 1K.txt
 ./tlf 10000 > 10K.txt
