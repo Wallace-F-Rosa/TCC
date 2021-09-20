aa5f03b9ae72b68b70097bd8601b47ae3c372945
Wallace Ferancini Rosa <wferancini@gmail.com>
2021-09-20 16:25:12 -0300

    testes: passando número de threads como parâmetro

--- test100.sh
+++ test100.sh
@@ -5,7 +5,7 @@ mkdir -p "testes/${1}"
 cd "testes/${1}"
 python ../../redes_reg_genes/redes_pesos/load_graph.py "../../redes_reg_genes/redes_bio/100-200/${1}/expr/expressions.ALL.txt" 1
 python ../../TCC/generate_code.py "expressions_pesos.txt"
-nvcc -w -DTHREADS=256 -arch=sm_37 -Xcompiler -fopenmp -Wno-deprecated-gpu-targets -lcurand -o tlf tlf.cu
+nvcc -w -DTHREADS=$2 -arch=sm_37 -Xcompiler -fopenmp -Wno-deprecated-gpu-targets -lcurand -o tlf tlf.cu
 ./tlf 1000 > 1K.txt
 ./tlf 10000 > 10K.txt
 ./tlf 100000 > 100K.txt
