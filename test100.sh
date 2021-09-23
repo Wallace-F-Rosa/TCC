6f371e972cf6d163700d39596b03777d38f24ad0
Wallace Ferancini Rosa <wferancini@gmail.com>
2021-09-23 14:58:33 -0300

    fix test100.sh não foi ajustado para equações booleanas

--- test100.sh
+++ test100.sh
@@ -4,7 +4,12 @@
 mkdir -p "testes/${1}"
 cd "testes/${1}"
 python ../../redes_reg_genes/redes_pesos/load_graph.py "../../redes_reg_genes/redes_bio/100-200/${1}/expr/expressions.ALL.txt" 1
-python ../../TCC/generate_code.py "expressions_pesos.txt"
+if [[ $# -ge 3 ]]
+then
+    python ../../TCC/generate_code.py "../../redes_reg_genes/redes_bio/40-100/${1}/expr/expressions.ALL.txt" $3
+else
+    python ../../TCC/generate_code.py "expressions_pesos.txt"
+fi
 nvcc -w -DTHREADS=$2 -arch=sm_37 -Xcompiler -fopenmp -Wno-deprecated-gpu-targets -lcurand -o tlf tlf.cu
 ./tlf 1000 > 1K.txt
 ./tlf 10000 > 10K.txt
