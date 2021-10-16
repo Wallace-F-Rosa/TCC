b3fe351f8305faea3726e4d860ff405c4e7961d2
Wallace Ferancini Rosa <wferancini@gmail.com>
2021-10-16 15:48:47 -0300

    modificação em testes para simular em cpu e gpu

--- test.sh
+++ test.sh
@@ -6,9 +6,9 @@ cd "testes/${1}"
 python ../../redes_reg_genes/redes_pesos/load_graph.py "../../redes_reg_genes/redes_bio/40-100/${1}/expr/expressions.ALL.txt" 1
 if [[ $# -ge 3 ]]
 then
-    python ../../TCC/generate_code.py "../../redes_reg_genes/redes_bio/40-100/${1}/expr/expressions.ALL.txt" $3
+    python ../../TCC/generate_code.py "../../redes_reg_genes/redes_bio/40-100/${1}/expr/expressions.ALL.txt" $3 --test-both
 else
-    python ../../TCC/generate_code.py "expressions_pesos.txt"
+    python ../../TCC/generate_code.py "expressions_pesos.txt" --test-both
 fi
 nvcc -w -DTHREADS=$2 -arch=sm_37 -Xcompiler -fopenmp -Wno-deprecated-gpu-targets -lcurand -o tlf tlf.cu
 ./tlf 1000 > 1K.txt
--- test100.sh
+++ test100.sh
@@ -6,11 +6,11 @@ cd "testes/${1}"
 python ../../redes_reg_genes/redes_pesos/load_graph.py "../../redes_reg_genes/redes_bio/100-200/${1}/expr/expressions.ALL.txt" 1
 if [[ $# -ge 3 ]]
 then
-    python ../../TCC/generate_code.py "../../redes_reg_genes/redes_bio/40-100/${1}/expr/expressions.ALL.txt" $3
+    python ../../generate_code.py "../../redes_reg_genes/redes_bio/100-200/${1}/expr/expressions.ALL.txt" -b --test-both
 else
-    python ../../TCC/generate_code.py "expressions_pesos.txt"
+    python ../../generate_code.py "expressions_pesos.txt" --test-both
 fi
-nvcc -w -DTHREADS=$2 -arch=sm_37 -Xcompiler -fopenmp -Wno-deprecated-gpu-targets -lcurand -o tlf tlf.cu
+g++ -fopenmp -o tlf tlf.cpp
 ./tlf 1000 > 1K.txt
 ./tlf 10000 > 10K.txt
 ./tlf 100000 > 100K.txt
